0470ce8a970622770c7cfb22bb7f0852e9c287b7
ShrutikaBhosale <141473148+ShrutikaBhosale@users.noreply.github.com>
2025-01-18 22:40:24 +0530

    net balance feature added

---
+++ src/generateLog.js
@@ -0,0 +1,49 @@
+import data from "../data/transactionLog.json" with {type: 'json'}
+
+const sub = (a, b) => a - b;
+
+const mergeWith = (o1, o2, f) => {
+  const res = { ...o1 };
+  for (const [k, v] of Object.entries(o2)) {
+    res[k] = res[k] ? f(res[k], v) : v;
+  }
+  return res;
+}
+
+const fetchAccounts = (transactions) => {
+  return structuredClone(transactions.reduce((acc, transaction) => {
+    acc[transaction.account] = 0;
+    return acc;
+  }, {}));
+}
+
+const fetchTransactionData = (transactions,type) => {
+  return transactions.filter(transaction => transaction.type === type)
+}
+
+const calculateTotalAmount = ( transactions) => {
+  const accounts = fetchAccounts(transactions);
+  transactions.forEach(transaction => {
+    accounts[transaction.account] += transaction.amount
+  })
+  return accounts;
+} 
+
+const netBalance = (transactions) => {
+
+  const accounts = fetchAccounts(transactions);
+  const acc = fetchAccounts(transactions);
+  
+  const credits = fetchTransactionData(transactions,'credit')
+  const debits = fetchTransactionData(transactions, 'debit')
+  
+ const creditedAmount = calculateTotalAmount(credits)
+  const debitedAmount = calculateTotalAmount(debits)
+  console.log(mergeWith(creditedAmount,debitedAmount,sub))
+}
+const main = () => {
+  const transactions = data.transactions;
+  return netBalance(transactions)
+}
+
+console.log(main());
